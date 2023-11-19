28d244a23c5a2ee61fc2c5cdd9a0cf4bc3f55565
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-11-20 01:47:49 +0700

    feat: metadata for dashboard

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+
+import React, { ReactNode } from "react";
+
+export const metadata: Metadata = {
+	metadataBase: new URL("https://dailyblog-demo.vercel.app/"),
+
+	title: {
+		template: "%s | Dashboard",
+		default: "Dashboard",
+	},
+	authors: {
+		name: "chensokheng",
+	},
+
+	description:
+		"Empower your decision-making with our intuitive dashboard. Gain valuable insights at a glance with interactive visualizations and real-time analytics. Our dashboard provides a centralized hub for monitoring key metrics, tracking progress, and making data-driven decisions. Streamline your workflow, enhance collaboration, and stay ahead of the curve with customizable widgets and personalized dashboards. Experience the power of data in a user-friendly interface designed to optimize productivity and drive results.",
+	openGraph: {
+		title: "Dashboard",
+		description:
+			"Empower your decision-making with our intuitive dashboard. Gain valuable insights at a glance with interactive visualizations and real-time analytics. Our dashboard provides a centralized hub for monitoring key metrics, tracking progress, and making data-driven decisions. Streamline your workflow, enhance collaboration, and stay ahead of the curve with customizable widgets and personalized dashboards. Experience the power of data in a user-friendly interface designed to optimize productivity and drive results.",
+		url: "https://dailyblog-demo.vercel.app/",
+		siteName: "Daily Blog",
+		images: "/og-dashboard.png",
+		type: "website",
+	},
+	keywords: ["daily web coding", "chensokheng", "dailywebcoding"],
+};
+
+export default function Layout({ children }: { children: ReactNode }) {
+	return <>{children}</>;
+}
