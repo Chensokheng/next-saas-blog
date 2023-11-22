6996f8720fcf1075b569dd36351140e0e5b15259
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-11-23 00:39:16 +0700

    feat: middleware only dashborad

--- middleware.ts
+++ middleware.ts
@@ -79,5 +79,8 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico|(home)).*)"],
+	matcher: [
+		// "/((?!api|_next/static|_next/image|favicon.ico).*)",
+		"/dashboard",
+	],
 };
