ca586b7d4e68d7e313988a96fd9bb061211659c0
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-11-23 11:24:03 +0700

    feat: update api

--- app/api/all/route.ts
+++ app/api/all/route.ts
@@ -7,6 +7,13 @@ export async function GET(request: Request) {
 		process.env.NEXT_PUBLIC_SUPABASE_URL!,
 		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 	);
-	const result = await supabase.from("blog").select("id");
-	return Response.json({ ...result });
+	const { searchParams } = new URL(request.url);
+	const query = searchParams.get("query");
+
+	if (query) {
+		const result = await supabase.from("blog").select(query!);
+		return Response.json({ ...result });
+	}
+
+	return Response.json({});
 }
