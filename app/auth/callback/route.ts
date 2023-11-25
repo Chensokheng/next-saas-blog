135626087022f30ae9136c6f169407181a36bd7e
Chensokheng <52232579+Chensokheng@users.noreply.github.com>
2023-11-25 10:12:53 +0700

    fix: supabase

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,6 +1,7 @@
-import { createSupabaseServerClient } from "@/lib/supabase/";
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { Database } from "@/lib/types/supabase";
 export async function GET(request: Request) {
 	const requestUrl = new URL(request.url);
 	const isAuth = cookies().get("supabase-auth-token");
@@ -14,7 +15,24 @@ export async function GET(request: Request) {
 	const next = searchParams.get("next") ?? "/";
 
 	if (code) {
-		const supabase = await createSupabaseServerClient();
+		const cookieStore = cookies();
+		const supabase = createServerClient<Database>(
+			process.env.NEXT_PUBLIC_SUPABASE_URL!,
+			process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+			{
+				cookies: {
+					get(name: string) {
+						return cookieStore.get(name)?.value;
+					},
+					set(name: string, value: string, options: CookieOptions) {
+						cookieStore.set({ name, value, ...options });
+					},
+					remove(name: string, options: CookieOptions) {
+						cookieStore.set({ name, value: "", ...options });
+					},
+				},
+			}
+		);
 
 		const { error } = await supabase.auth.exchangeCodeForSession(code);
 
--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -14,12 +14,6 @@ export async function createSupabaseServerClient() {
 				get(name: string) {
 					return cookieStore.get(name)?.value;
 				},
-				set(name: string, value: string, options: CookieOptions) {
-					cookieStore.set({ name, value, ...options });
-				},
-				remove(name: string, options: CookieOptions) {
-					cookieStore.set({ name, value: "", ...options });
-				},
 			},
 		}
 	);
